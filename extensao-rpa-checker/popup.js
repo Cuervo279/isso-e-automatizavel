b2448d149359c005acd3aded2ff43a867e79aa6e
Leonardo Cuervo <115048855+Cuervo279@users.noreply.github.com>
2026-01-29 19:53:29 -0300

    Delete extensao-rpa-checker directory

--- extensao-rpa-checker/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("analisar").onclick = async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
-  });
-};
